5986248871f15780e9dce1f93f9fdd844edc2467
Thidaporn Kijkamjai <jinglejill@me.com>
2018-06-05 07:26:40 +0700

    Initial Commit

---
+++ JummumShopUrl/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  JummumShopUrl
+//
+//  Created by Thidaporn Kijkamjai on 5/6/2561 BE.
+//  Copyright © 2561 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Document : UIDocument
+    
+@end
---
+++ JummumShopUrl/DocumentViewController.h
@@ -0,0 +1,15 @@
+//
+//  DocumentViewController.h
+//  JummumShopUrl
+//
+//  Created by Thidaporn Kijkamjai on 5/6/2561 BE.
+//  Copyright © 2561 Appxelent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DocumentViewController : UIViewController
+
+@property (strong) UIDocument *document;
+
+@end
